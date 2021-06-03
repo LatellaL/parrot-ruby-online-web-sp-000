6747ce38d601902c38b9f9c03d3d90d998d17857
Latella L <lt.latella@gmail.com>
2021-06-03 00:25:46 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,7 +1,7 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
 
-def parrot(string = "Sqwauk!")
+def parrot(string = "Sqwuak!")
   puts "#{string}"
    "#{string}"
 end   
